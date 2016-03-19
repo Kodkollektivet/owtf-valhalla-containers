fb4f861b4927367342f6fef7fab732041a31bcd3
Robin Karlsson <rk222ev@student.lnu.se>
2016-03-19 11:27:33 +0100

    Change subprocess to run in background

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python
 
 import configparser
-from subprocess import check_output
+import os
+from subprocess import Popen, PIPE
 from flask import Flask, request, jsonify, url_for
 
 app = Flask(__name__)
 
+# Process should be a que to enable multiple subprocesses
+process = None
+
+
 @app.route("/")
 def index():
-    return jsonify({ "description": read_config('description')})
+    """Returns information about the command and possible routes"""
+    return jsonify({"description": read_config('description')})
+
 
 @app.route("/run")
 def run():
-    command = read_config('command')
-    res = check_output([command, "-l"])
-    print(res)
-    return jsonify({ "response": str(res) })
+    """Starts the subproccess"""
+    global process
+
+    script = read_config('command')
+    process = Popen([script], stdout=PIPE)
+    return jsonify({"message": "Subprocess started"})
+
+
+@app.route("/result")
+def result():
+    """Read the result from the subprocess """
+    global process
+
+    if not process:
+        return jsonify({"response": "No running proccess"})
+    elif not process.poll() == 0:
+        return jsonify({"response": "Process still running"})
+
+    res = process.communicate()
+    process.kill()
+    process = None
+    return jsonify({"response": str(res[0])})
+
 
 def read_config(key):
+    """Read key from config"""
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config['main'][key]
@@ -25,4 +52,3 @@ def read_config(key):
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-
